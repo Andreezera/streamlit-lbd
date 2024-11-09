a6989b2ba0f4e59307324bf8e81374858199a273
Andre Dionisio <andredionisio1@hotmail.com>
2024-11-09 02:07:47 -0300

    primeiras consultas

--- app.py
+++ app.py
@@ -2,26 +2,64 @@ import streamlit as st
 import mysql.connector
 import pandas as pd
 
-st.header("Primeiro titulo")
+# Configuração de cabeçalho da página
+st.set_page_config(page_title="Escolas e Dados", layout="wide")
+st.title("Análise de Escolas")
 
-st.markdown("""
-            Primeira **linha** *aqui*
-            """)
+# Conexão com o banco de dados
+def create_connection():
+    return mysql.connector.connect(
+        host='sql.freedb.tech',
+        user='freedb_freedb-user',
+        password='*dtvFHJ7hDq%7&G',
+        port=3306,
+        db='freedb_escola-db',
+        auth_plugin='mysql_native_password'
+    )
 
-conn = mysql.connector.connect(host='sql.freedb.tech', 
-                               user='freedb_freedb-user', 
-                               password='*dtvFHJ7hDq%7&G', 
-                               port=3306, 
-                               db='freedb_escola-db', 
-                               auth_plugin='mysql_native_password')
+# Função para executar consulta SQL e retornar um DataFrame
+def execute_query(query):
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute(query)
+    data = cursor.fetchall()
+    df = pd.DataFrame(data, columns=cursor.column_names)
+    cursor.close()
+    conn.close()
+    return df
 
-cursor = conn.cursor()
+# Exibição de escolas
+st.subheader("Informações das Escolas")
+escolas_query = """
+    SELECT 
+        e.NO_ENTIDADE, 
+        e.TP_SITUACAO_FUNCIONAMENTO, 
+        e.CO_MUNICIPIO, 
+        e.TP_LOCALIZACAO, 
+        e.TP_DEPENDENCIA 
+    FROM 
+        escolas e
+"""
+df_escolas = execute_query(escolas_query)
+st.dataframe(df_escolas, use_container_width=True)
 
-cursor.execute("select * from escolas;")
-res = cursor.fetchall()
-df = pd.DataFrame(res, columns=cursor.column_names)
-
-st.write(df)
-
-st.sidebar.header("Meu Sidebar")
-st.sidebar.radio("Meu Radio", df['NO_ENTIDADE'].unique())
+# Totais de matrículas, professores e turmas
+st.subheader("Totais de Matrículas, Professores e Turmas por Escola")
+totais_query = """
+    SELECT 
+        e.NO_ENTIDADE, 
+        COUNT(DISTINCT m.ID_MATRICULA) AS Total_Alunos, 
+        COUNT(DISTINCT d.CO_PESSOA_FISICA) AS Total_Professores
+    FROM 
+        escolas e
+    LEFT JOIN 
+        matriculas m ON e.CO_ENTIDADE = m.CO_ENTIDADE
+    LEFT JOIN 
+        docentes d ON e.CO_ENTIDADE = d.CO_ENTIDADE
+    GROUP BY 
+        e.NO_ENTIDADE, e.CO_ENTIDADE
+    ORDER BY 
+        e.NO_ENTIDADE ASC;
+"""
+df_totais = execute_query(totais_query)
+st.dataframe(df_totais, use_container_width=True)
